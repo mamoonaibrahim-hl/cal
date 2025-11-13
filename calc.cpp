53d130770b874813f28ed02c2ed3d022cf260b31
mamoonaibrahim-spec <mamoonaibrahim10c@gmail.com>
2025-11-13 20:38:54 +0500

    Add extra cout outputs (modulus, average, max/min, squares)

--- calc.cpp
+++ calc.cpp
@@ -9,5 +9,12 @@ int main()
 	cout<<"Subtraction of a and b: "<<a-b<<endl;
 	cout<<"Multiplication of a and b: "<<a*b<<endl;
 	cout<<"Division of a and b: "<<a/b<<endl;
+	cout<<"Modulus of a and b: "<<a%b<<endl;
+	cout<<"Average of a and b: "<<(a + b) / 2.0<<endl;
+	cout<<"Maximum of a and b: "<<(a > b ? a : b)<<endl;
+	cout<<"Minimum of a and b: "<<(a < b ? a : b)<<endl;
+	cout<<"Square of a: "<<a * a<<endl;
+	cout<<"Square of b: "<<b * b<<endl;
+	cout<<"this shit works";
 	return 0;
 }
