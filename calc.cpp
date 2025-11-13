d0286609a14576100906cd1fc72e681b7cea9ea1
mamoonaibrahim-hl <mamoonaibrahim10c@gmail.com>
2025-11-13 19:43:56 +0500

    initial commit

---
+++ calc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the values of a and b: ";
+	cin>>a>>b;
+	cout<<"Addition of a and b: "<<a+b<<endl;
+	cout<<"Subtraction of a and b: "<<a-b<<endl;
+	cout<<"Multiplication of a and b: "<<a*b<<endl;
+	cout<<"Division of a and b: "<<a/b<<endl;
+	return 0;
+}
